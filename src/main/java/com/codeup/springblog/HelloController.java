f1b8611906a5d8329a1f6054a75aaeaf249f60db
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-02-22 15:32:38 -0600

    completed spring controller exercies

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,27 @@
+package com.codeup.springblog;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello{name}") //{name} will be the path variable
+    @ResponseBody // will return the value wanted for the GetMapping
+    public String hello(@PathVariable String name) { //@PathVariable annotation to allow name
+        //return "hello from spring!";
+        return "Hello, " + name + " !";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
---
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -0,0 +1,32 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("/add/3/and/4")
+    @ResponseBody
+    public String add() {
+        return "3 + 4 = " + (3+4);
+    }
+
+    @GetMapping("/subtract/3/from/10")
+    @ResponseBody
+    public String subtract() {
+        return "10 - 3 = " + (10-3);
+    }
+
+    @GetMapping("/multiply/4/and/5")
+    @ResponseBody
+    public String multiply() {
+        return "5 * 4 = " + (4*5);
+    }
+
+    @GetMapping("/divide/6/by/3")
+    @ResponseBody
+    public String divide() {
+        return "6 / 3 = " + (6/3);
+    }
+}
