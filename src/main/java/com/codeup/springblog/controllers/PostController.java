fbdf16290ba2c7820460035904eee11173497ae8
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-03-02 09:14:17 -0600

    completed services walkthrough

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,14 +1,12 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
-import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.UserRepository;
+import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 //@GetMapping is shorter way, already hanbdling the GET request
 //@PostMapping shorter way of handling POST request
@@ -17,9 +15,12 @@ import java.util.List;
 public class PostController {
     private PostRepository postDao;
     private UserRepository userDao;
-    public PostController(PostRepository postDao, UserRepository userDao) {
+    private final EmailService emailService;
+
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
         this.postDao = postDao;
         this.userDao = userDao;
+        this.emailService = emailService;
     }
 
     @GetMapping ("/posts")
@@ -62,7 +63,7 @@ public class PostController {
         //Post newPost = new Post(title, body);
         newPost.setUser(userDao.getById(1L));
         postDao.save(newPost);
-
+        emailService.prepareAndSend(newPost,"New post was created", "You posted to the blog");
         return "redirect:/posts";
     }
 
